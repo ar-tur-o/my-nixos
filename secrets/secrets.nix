843cf7c9acf90dad88df7073a5143ea5049a85c5
arturos@optiplex <salgadoarturo1219@gmail.com>
2026-06-20 01:55:16 -0700

    chore: regen keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,14 +3,17 @@ let
   # > ssh-keygen -t ed25519
   optiplex = {
     pub = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAuSaFQO8SDTChpK+KIhoix+BNV1lQyN4m6BSXm7ePkZ"; 
-    arturos.pub = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICjxxOnhHTKch2L4THT4bnTQ1+HK2d1KKhdsG7P6IRdM";
   };
+
+  arts-pc = {
+    pub = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIz/ya6TFHpUagr7WoGYgK5ny53lVsrhLyPEd6G0y0IL";
+  };
+
   in {
   # example
-  "api/key.age".publicKeys = [optiplex.arturos.pub];
   "nextcloud-admin.age".publicKeys = [optiplex.pub];
   "cloudflare-tunnel.age".publicKeys = [optiplex.pub];
   "immich.age".publicKeys = [optiplex.pub];
   "playit.age".publicKeys = [optiplex.pub];
-  "tailscale.age".publicKeys = [optiplex.pub];
+  "tailscale.age".publicKeys = [optiplex.pub arts-pc.pub];
 }
