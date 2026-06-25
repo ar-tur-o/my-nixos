0d37a6dfb859670178dcbf88548234891c4a55c0
arturos@arts-pc <salgadoarturo1219@gmail.com>
2026-06-24 22:36:10 -0700

    made tmux default in bash shell

--- profiles/core/tmux.nix
+++ profiles/core/tmux.nix
@@ -1,8 +1,13 @@
-{pkgs, ...}:
 {
   programs.tmux = {
     enable = true;
     terminal = "tmux-256color";
     extraConfig = ''set -as terminal-overrides ",xterm-256color:RGB"'';
   };
+
+  programs.bash.initExtra = ''
+    if [[ -z "$TMUX" ]] && [[ -n "$PS1" ]] && command -v tmux &>/dev/null; then
+      exec tmux new-session -A -s main
+    fi
+  '';
 }
