04988bc8791817222f92621a2c24d60eb2454870
arturos@optiplex <salgadoarturo1219@gmail.com>
2026-07-10 05:38:54 -0700

    updated nextcloud for performance

--- hosts/optional/server/nextcloud.nix
+++ hosts/optional/server/nextcloud.nix
@@ -3,32 +3,38 @@
   options.myHost.nextcloudServer.enable = lib.mkEnableOption "Enable Nextcloud server on this device";
 
   config = lib.mkIf config.myHost.nextcloudServer.enable {
+    services.postgresql = {
+      enable = true;
+      ensureDatabases = [ "nextcloud" ];
+      ensureUsers = [
+        {
+          name = "nextcloud";
+          ensureDBOwnership = true;
+        }
+      ];
+    };
+
     services.nextcloud = {
       enable = true;
-      package = pkgs.nextcloud32;
-      hostName = "nextcloud.computer-day.com";
       https = true;
+      database.createLocally = true;
+      configureRedis = true;
+      package = pkgs.nextcloud33;
+      hostName = "nextcloud.computer-day.com";
       datadir = "/srv/nextcloud";
-      config.adminpassFile = config.age.secrets.nextcloud-admin.path;
-      config.dbtype = "sqlite";
+      config = {
+        adminpassFile = config.age.secrets.nextcloud-admin.path;
+        dbtype = "pgsql";
+        dbname = "nextcloud";
+        dbuser = "nextcloud";
+        dbhost = "/run/postgresql";
+        trustedProxies = ["127.0.0.1"];
+      };
     };
 
     myHost.cloudflared = {
       enable = true;
       ingress = {"nextcloud.computer-day.com" = "http://localhost:80";};
-    };
-
-    services.cloudflared = {
-      enable = true;
-      tunnels = {
-        "656d5403-a187-42a7-a57e-f2ec3e7cfd39" = {
-          credentialsFile = config.age.secrets.cloudflare-tunnel.path;
-          ingress = {
-            "immich.computer-day.com" = "http://localhost:2283";
-          };
-          default = "http_status:404";
-        };
-      };
     };
 
     networking.firewall.allowedTCPPorts = [80 443];
